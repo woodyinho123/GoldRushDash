8fb42f7db6ffe181a3a7ae2a13685920c7dc3e98
woodyinho123 <cailean2010@live.co.uk>
2025-12-01 02:11:57 +0000

    gg
    gg

--- Assets/Scripts/RockTriggerZone.cs
+++ Assets/Scripts/RockTriggerZone.cs
@@ -2,11 +2,33 @@ using UnityEngine;
 
 public class RockTriggerZone : MonoBehaviour
 {
-    public RockLinearMotion rockToDrop;
+    [Tooltip("The FallingRock this trigger will drop.")]
+    public FallingRock rockToDrop;
+
+    [Tooltip("Trigger only once, even if the player re-enters.")]
+    public bool triggerOnce = true;
+
+    private bool hasTriggered = false;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (!other.CompareTag("Player")) return;
-        if (rockToDrop != null) rockToDrop.Drop();
+        // Only react to the player
+        if (!other.CompareTag("Player"))
+            return;
+
+        // If we already triggered and we only want it once, ignore
+        if (triggerOnce && hasTriggered)
+            return;
+
+        if (rockToDrop != null)
+        {
+            Debug.Log("RockTriggerZone: Player entered, dropping rock " + rockToDrop.name);
+            rockToDrop.Drop();
+            hasTriggered = true;
+        }
+        else
+        {
+            Debug.LogWarning("RockTriggerZone: No rockToDrop assigned on " + name);
+        }
     }
 }
